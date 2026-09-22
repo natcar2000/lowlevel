dd0c369f7003e98886347d9e4eebe1df960336a5
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-22 10:37:47 -0300

    Refactor memory management by removing free_memory
    Removed free_memory function and its bindings.

--- lowlevel/memory/memory.py
+++ lowlevel/memory/memory.py
@@ -6,14 +6,7 @@ memory = ctypes.CDLL("./memory.dll")
 memory.allocate_memory.argtypes = [ctypes.c_size_t]
 memory.allocate_memory.restype = ctypes.c_void_p
 
-memory.free_memory.argtypes = [ctypes.c_void_p]
-memory.free_memory.restype = None
-
 
 def allocate(size):    
    address = memory.allocate_memory(size)
    return address
-
-
-def free(address):
-   memory.free_memory(address)
