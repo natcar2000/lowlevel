54676cddff9ce6dd5cd33b8334751b0c016963ae
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-23 15:00:21 -0300

    Update memory.py

--- lowlevel/memory/memory.py
+++ lowlevel/memory/memory.py
@@ -6,7 +6,59 @@ memory = ctypes.CDLL("./memory.dll")
 memory.allocate_memory.argtypes = [ctypes.c_size_t]
 memory.allocate_memory.restype = ctypes.c_void_p
 
+memory.free_memory.argtypes = [ctypes.c_void_p]
+memory.free_memory.restype = ctypes.c_int
 
-def allocate(size):    
-   address = memory.allocate_memory(size)
-   return address
+memory.get_allocation_id.argtypes = [ctypes.c_void_p]
+memory.get_allocation_id.restype = ctypes.c_int
+
+memory.write_memory.argtypes = [ctypes.c_int, ctypes.c_size_t, ctypes.c_void_p]
+memory.write_memory.restype = ctypes.c_void_p
+
+
+def validate_address(address):
+    if type(address) != int:
+        raise TypeError("Address must be a valid integer.")
+
+
+def validate_size(size):
+    if type(size) != int:
+        raise TypeError("Size must be an integer.") 
+   
+    if size <= 0:
+        raise ValueError("Size must be greater than zero.")
+        
+
+def allocate(size): 
+    validate_size(size)    
+
+    address = memory.allocate_memory(size)
+
+    return address
+
+
+def free(address):	
+    validate_address(address)
+    return memory.free_memory(address)   
+
+
+def get_id(address):
+    validate_address(address)
+    return memory.get_allocation_id(address) 
+
+
+def write(id, size, data):
+    validate_size(size)
+    
+    if type(data) != bytes:
+        raise TypeError("Data must be bytes.")
+
+    if len(data) == 0:
+        raise ValueError("Data must not be empty.")
+
+    if size > len(data):
+        raise ValueError("Size cannot be greater than data length.")
+
+    buffer = ctypes.create_string_buffer(data) 
+ 
+    return memory.write_memory(id, size, ctypes.cast(buffer, ctypes.c_void_p))
