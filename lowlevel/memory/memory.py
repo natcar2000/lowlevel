234fd69c780cf7707166cbea2badb178e8e41804
Natanael Rodrigues <115945833+natcar2000@users.noreply.github.com>
2026-09-24 13:43:20 -0300

    Update memory.py

--- lowlevel/memory/memory.py
+++ lowlevel/memory/memory.py
@@ -15,10 +15,24 @@ memory.get_allocation_id.restype = ctypes.c_int
 memory.write_memory.argtypes = [ctypes.c_int, ctypes.c_size_t, ctypes.c_void_p]
 memory.write_memory.restype = ctypes.c_void_p
 
+memory.read_memory.argtypes = [ctypes.c_int, ctypes.c_size_t]
+memory.read_memory.restype = ctypes.c_void_p
+
 
 def validate_address(address):
     if type(address) != int:
         raise TypeError("Address must be a valid integer.")
+
+
+def validate_data(data, size):
+    if type(data) != bytes:
+        raise TypeError("Data must be bytes.")
+
+    if len(data) == 0:
+        raise ValueError("Data must not be empty.")
+
+    if size > len(data):
+        raise ValueError("Size cannot be greater than data length.")
 
 
 def validate_size(size):
@@ -49,16 +63,16 @@ def get_id(address):
 
 def write(id, size, data):
     validate_size(size)
+    validate_data(data, size)
     
-    if type(data) != bytes:
-        raise TypeError("Data must be bytes.")
-
-    if len(data) == 0:
-        raise ValueError("Data must not be empty.")
-
-    if size > len(data):
-        raise ValueError("Size cannot be greater than data length.")
-
     buffer = ctypes.create_string_buffer(data) 
  
     return memory.write_memory(id, size, ctypes.cast(buffer, ctypes.c_void_p))
+
+
+def read(id, size):
+    validate_size(size)
+    
+    result = memory.read_memory(id, size)
+    
+    return ctypes.string_at(result, size)
